b0efcafcdba40b3c3e28f128925af1c05dc32c32
hanktutt <35299445+hanktutt@users.noreply.github.com>
2018-01-17 15:21:11 -0500

    This assignment was a little bit about myself

--- 01 hello.py
+++ 01 hello.py
@@ -7,3 +7,9 @@ assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 print('Hello, Class!')
 
 print('hello my name is hank')
+
+print('currently my favorite game is star wars battlefront on xbox because I am a huge star wars fan, but my favorite game is always changing')
+print('I am concerned I will fall behind because of lack of experience')
+print('I am excited to gain experience to avoid falling behind in other classes or in my career')
+print('my stackoverflow ID number is user:9198868')
+print('the url to my github profile is https://github.com/hanktutt')
